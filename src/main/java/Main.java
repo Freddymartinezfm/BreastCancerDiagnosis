1b4426d1a5f61c37e120ba94a41f524de19fd2ba
freddymartinezfm1 <38080761+freddymartinezfm1@users.noreply.github.com>
2020-11-05 14:40:37 -0600

    logging to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,10 @@ public class Main {
 
 	public static void main(String[] args)  {
 		// TODO create controller to manage the record and send back to client to send to peers
-		Logger testLogger = LogManager.getLogger(Main.class.getName());
+		Logger logger = LogManager.getLogger(Main.class.getName());
+		logger.trace("trace");
 
-		testLogger.debug("basicLogToConsole for {}", testLogger);
-		testLogger.debug("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
-		testLogger.error("basicLogToConsole");
+
 
 
 
--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -1,5 +1,6 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.Supplier;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -7,9 +8,16 @@ import org.junit.jupiter.api.DisplayName;
 public class LoggerTest {
 
 	@Test
-	@DisplayName("Log should be printed to console ")
-	public void basicLogToConsole(){
-
+	@DisplayName("Displays logger levels printed to console from lowest priority to highest ")
+	public void loggerLevel() {
+		Logger logger = LogManager.getLogger(LoggerTest.class.getName());
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.trace("trace");
+		logger.error("error");
 
 	}
 }
+
+
